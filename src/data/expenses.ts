53d0c47027e428385e14e88286cfaf24ae5db41e
Maxime FLAMENT <maximeflamentweb@gmail.com>
2023-08-21 20:51:21 +0200

    First commit, setting up project skeleton, ui, types and initial data

---
+++ src/data/expenses.ts
@@ -0,0 +1,6 @@
+import { Expense } from '@/types/Expense.types'
+
+export const expenses: Expense[] = [
+  { id: '728ed52f', amount: 100, category: 1, createdAt: new Date('August 21, 2023, 20:11:56') },
+  { id: '728ed52f', amount: 100, category: 1, createdAt: new Date('August 18, 2023, 13:43:14') },
+]
